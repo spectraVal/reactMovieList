f2c4d7191c487416a028438207beb96a6658ccaf
rival <muhammadrivalmau@gmail.com>
2025-08-07 10:45:32 +0700

    Fixing Logo

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
                 rel="noopener noreferrer"
               >
                 <img
-                  src="/public/LogoTMDB.svg"
+                  src={`${process.env.PUBLIC_URL}/LogoTMDB.svg`}
                   alt="TMDB Logo"
                   className="h-4 w-auto inline-block align-middle object-contain"
                 />
